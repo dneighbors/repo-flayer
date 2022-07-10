1a6aec063f25af3d7897cdfbae54a65b16307a35
Derek Neighbors <dneighbo@gmail.com>
2022-07-09 23:34:59 -0700

    add ability to load repos from git hub

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,2 +1,22 @@
 class Repository < ApplicationRecord
+  require 'octokit'
+
+
+  def self.load_from_github
+    Octokit.auto_paginate = true
+    octo_client = Octokit::Client.new(:access_token => ENV["GITHUB_KEY"])
+
+    organizations = octo_client.organizations
+    organizations.each do |organization|
+      if organization[:login] == "StrongMind"
+        @organization_repositories = octo_client.org_repos(organization[:login], {:type => 'all'})
+      end
+    end
+
+    @organization_repositories.each do |organization_repository|
+      # puts organization_repository[:name]
+      self.upsert({name: organization_repository[:name], description: organization_repository[:description]})
+    end
+
+  end
 end
